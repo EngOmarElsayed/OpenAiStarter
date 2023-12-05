b155951beb91ba8ff15da70663d7cf5ec1d8a76c
Omar Elsayed <eng.omar.elsayed@hotmail.com>
2023-12-05 12:29:52 +0200

    Initial Commit

---
+++ OpenAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenAI
+//
+//  Created by Eng.Omar Elsayed on 05/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenAI/OpenAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAIApp.swift
+//  OpenAI
+//
+//  Created by Eng.Omar Elsayed on 05/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct OpenAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
